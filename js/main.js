5ae08d864fe873bccd546b30d9fe99a4a03b9994
cem aytac <cenko@MacBook-Air-3.local>
2020-08-10 22:47:26 -0400

    buttonDelay function working

--- js/main.js
+++ js/main.js
@@ -19,11 +19,19 @@ adviceBtn.addEventListener('click', () => {
     // get advice from slip array and push it into quote array
     .then((data) => {
       quotes.push(data.slip.advice);
-      console.log(data.slip.advice);
+      render();
     })
     .catch((err) => {
       console.log(err)
     })
+  // when button is clicked it must be disabled for about 4 seconds, because that's how long it takes for the API to call a different quote
+  function buttonDelay() {
+    adviceBtn.disabled = true;
+    setTimeout(function () {
+      adviceBtn.disabled = false;
+    }, 4500);
+  }
+  buttonDelay();
 })
 inspireBtn.addEventListener('click', () => {
   // promise
@@ -32,19 +40,26 @@ inspireBtn.addEventListener('click', () => {
     .then((response) => {
       return response.json();
     })
-    // get advice from object array and push it into quote array
+    // get content and author from array and push it into quote array
     .then((data) => {
       quotes.push(data.content, data.author);
-      console.log(data.content, data.author)
+      render();
     })
     .catch((err) => {
       console.log(err)
     })
 })
 
+luckyBtn.addEventListener('click', () => {
+  render();
+})
+
 // functions
-function appendDiv(advice) {
+
+
+function appendDiv(advice, content, author) {
   let adviceDiv = document.createElement('div')
+  let inspireDiv = document.createElement('div2')
   adviceDiv.innerHTML = ` <div class='card' id='advice'>
   <div class='card-body'>
   <blockquote class='blockquote mb-0'>
@@ -53,6 +68,14 @@ function appendDiv(advice) {
   </div>
   </div>
   `
+  inspireDiv.innerHtml = ` <div class='card' id='inspire'>
+<div class='card-body'>
+<blockquote class='blockquote mb-0'>
+<p>${content}</p>
+<footer class="blockquote-footer">${author}</footer>
+</blockquote>
+</div>
+</div>`
 
   box.appendChild(adviceDiv)
 }
